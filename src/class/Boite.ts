912d01b7a9d93565ee3f3566db5d5de37ff4864c
pytoncraft2 <pythoncraft2@gmail.com>
2022-10-19 01:17:41 +0200

    🧪 point d'interrogation disparition

--- src/class/Boite.ts
+++ src/class/Boite.ts
@@ -2,33 +2,35 @@ import Jeu from "../scenes/Jeu";
 
 export default class Boite extends Phaser.GameObjects.Rectangle {
     rect!: Phaser.GameObjects.Rectangle
+    point_interrogation!: Phaser.GameObjects.Text
 
     constructor(scene: Jeu, x: number, y: number, width: number, height: number, fillColor: any) {
         super(scene, x, y, width, height, fillColor);
 
-        const animationRect = scene.add.rectangle(x, y, width + 60, height + 60);
-		animationRect.scaleX = 0.50013023026658265;
-		animationRect.scaleY = 0.50013023026658265;
-		animationRect.isFilled = true;
-		animationRect.setAlpha(0.1);
-        this.rect = animationRect
+        // const animationRect = scene.add.rectangle(x, y, width + 60, height + 60);
+		// animationRect.scaleX = 0.50013023026658265;
+		// animationRect.scaleY = 0.50013023026658265;
+		// animationRect.isFilled = true;
+		// animationRect.setAlpha(0.1);
+        // this.rect = animationRect
         scene.add.existing(this);
 
-		const text_4 = scene.add.text(x, y, "", {});
-		text_4.scaleX = 1.8887616340344677;
-		text_4.scaleY = 1.8887616340344677;
-		text_4.setOrigin(0.5, 0.5);
-		text_4.text = "?";
+		const point_interrogation = scene.add.text(x, y, "", {});
+		point_interrogation.scaleX = 1.8887616340344677;
+		point_interrogation.scaleY = 1.8887616340344677;
+		point_interrogation.setOrigin(0.5, 0.5);
+		point_interrogation.text = "?";
+        this.point_interrogation = point_interrogation
 
 
-        scene.add.tween({
-            targets: animationRect,
-            scale: "+=0.04",
-            alpha: 0.2,
-            yoyo: true,
-            ease: "Bounce.easeIn",
-            repeat: -1
-        })
+        // scene.add.tween({
+            // targets: animationRect,
+            // scale: "+=0.04",
+            // alpha: 0.2,
+            // yoyo: true,
+            // ease: "Bounce.easeIn",
+            // repeat: -1
+        // })
 
 
     }
@@ -50,6 +52,10 @@ export default class Boite extends Phaser.GameObjects.Rectangle {
             this.scene.question.setVisible(true)
             //@ts-ignore
             this.scene.question.setDepth(100)
+            console.log(this.point_interrogation);
+            
+            this.point_interrogation.setText("OUIiiiiiiiiiiii")
+            // this.rect.setAlpha(0)
         }
 
 
